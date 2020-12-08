bf125bf2c6e6e28b8b71aeddf37546239073ff5a
Charlene <tantan123@live.cn>
2020-12-08 19:27:12 +0800

    modified user_info.cfg,doLoginOut.py

--- common/doLoginOut.py
+++ common/doLoginOut.py
@@ -24,6 +24,24 @@ class DoLoginOut:
         base_headers = cf.get_info_for_public('HEADERS')
         return  base_headers
 
+    def getUser(self):
+        """
+
+        :return:
+        """
+        cf = GetInfo(os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + "/config/user_info.cfg")
+        user_info = cf.get_info_for_public('USER_INFO')
+        return user_info
+
+    def getBody(self):
+        """
+
+        :return:
+        """
+        cf = GetInfo(os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + "/config/user_info.cfg")
+        body = cf.get_info_for_public('BODY')
+        return body
+
     def login(self):
         """
         1.获取验证码
@@ -36,25 +54,22 @@ class DoLoginOut:
         base_url_re = base_url.get('fcb_app_host_c')
 
         #url
-        data_code = {"phone": "15811258802", "type": 4, "by_voice_code": 0}
+        data_code = {
+                        "phone": DoLoginOut().getUser().get('phone'),
+                        "type": DoLoginOut().getBody().get('type'),
+                        "by_voice_code": DoLoginOut().getBody().get('by_voice_code')}
         data_login = {
-                        "code": "111111",
-                        "jpushId": "140fe1da9e1a4f54a69",
-                        "lbsCityCode": "440300",
-                        "phone": "15811258802",
-                        "registerFrom": "1",
-                        "siteCityCode": "440300",
-                        "hardware_version": "HUAWEI",
-                        "os_type": "Android",
-                        "os_version": "android_29",
-                        "terminalVersion":"v1.0.0",
-                        "clientType": "C_USER"
+                        "code": DoLoginOut().getUser().get('password'),
+                        "phone": DoLoginOut().getUser().get('phone'),
+                        "registerFrom": DoLoginOut().getBody().get('registerFrom'),
+                        "terminalVersion":DoLoginOut().getBody().get('terminalVersion'),
+                        "os_type": DoLoginOut().getBody().get('os_type'),
+                        "clientType": DoLoginOut().getBody().get('clientType')
                     }
-        res_code = requests.post(base_url_re+DoLoginOut().getUrl().get('getCode'),data=json.dumps(data_code),headers=DoLoginOut().getHeaders())
-        res_login = requests.post(base_url_re+DoLoginOut().getUrl().get('login'),data=json.dumps(data_login),headers=DoLoginOut().getHeaders())
+        res_code = requests.post(base_url_re+DoLoginOut().getUrl().get('getCode'), data=json.dumps(data_code), headers=DoLoginOut().getHeaders())
+        res_login = requests.post(base_url_re+DoLoginOut().getUrl().get('login'), data=json.dumps(data_login), headers=DoLoginOut().getHeaders())
 
         return res_login.json()
-
 
 if __name__ == '__main__':
     re = DoLoginOut().login()
